be941f707b8ebd62dc1dc92181f120ab61a7463f
duaiyumi <duaiyumi@gmail.com>
2017-09-26 21:05:10 +0800

    Update NumGuessGame_vip.py

--- NumGuessGame_vip.py
+++ NumGuessGame_vip.py
@@ -1,23 +1,45 @@
-#coding=utf-8
+# coding=utf-8
+    # 方便出中文版，采用utf-8字符
+
 import random
+secret = random.randint(0, 99)
+guess = int(0)
+tries = int(0)
+    # 加载随机数模块，生成了一个0-99的随机整数为答案。并将"猜的数字(guess)"和"次数(tries)"初始化。
 
-secret = random.randint(0,99)
-guess = 0
-tries = 0
+print("我是个可怕的海盗，罗伯特。我有个秘密！")
+print("猜一个0-99的整数，你有6的机会。")
+    # 开场语，"我是可怕的海盗罗伯特，我有个秘密！"
+    # 开场语，"猜一个0-99的整数，你有6次机会。"
 
-print ("Aha！我是可怕的海盗罗伯茨，我有一个秘密！")
-print ("猜一个0到99的数字。我给你6次机会。")
+while int(guess) != secret and tries < 6:
+    guess = input("选一个吧：")
+    # 游戏开始条件判定，当猜的数字不等于答案，且猜的次数有效。
 
-while guess != secret and tries < 6:
-    guess = input("选一个吧: ")
-    if int(guess) < secret:
-        print ("太低喽。")        
-    elif int(guess) > secret:
-        print ("太高了！")
+    if int(guess) == 724:
+        print(secret)
+    # 为"BF"预留的后门，直接查看随机整数。默认值为724。
+
+    elif int(guess) < 0 or int(guess) > 99:
+        print("超出范围啦！")
+    # 检测用户输入数值是否符合范围。
+
+    elif int(guess) < secret and -1 < int(guess) < 100:
+        print("太低了！")
+    # 用户输入数值低于随机整数。
+
+    elif int(guess) > secret and -1 < int(guess) < 100:
+        print("高了！高了！")
+    # 用户输入数值高于随机整数。
+
     tries = tries + 1
-        
-if guess == secret:
-    print ("Yeah！你猜对了！")
+    # 每结束一个回合，次数+1。
 
-else:
-    print "Answer:", secret
+if int(guess) == secret:
+    print("漂亮！你明白了吗？！我找到了我的秘密！")
+    # 用户猜对了，输出提示。
+
+elif tries == 6:
+    print("别再猜测了！下次好运，伙计！")
+    print("秘密号码是：", secret)
+    # 用户机会用完了，输出答案。
